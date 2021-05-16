210fd978ffa5b9e4d2c976330879131339662415
Brijesh Patel <b.patel9528@gmail.com>
2021-05-15 18:57:40 -0600

    Added functions for array_list

--- concepts/array_list/__main__.py
+++ concepts/array_list/__main__.py
@@ -1,9 +1,38 @@
 from array_list import ArrayList
 
-numbers = ArrayList(2, 3, 5, 6, 9, 10)
+numbers = ArrayList()
 
-
-for i in range(10):
+for i in range(11):
     numbers.append(i)
 
-print(" - ".join(map(lambda num: str(num), numbers)))
+numbers.append(11)
+
+numbers.append(12)
+
+print(numbers, f'length={len(numbers)}')
+
+print(0 in numbers, 1 in numbers, 8 in numbers, 14 in numbers, 19 in numbers)
+
+numbers.remove(11)
+
+numbers.remove(2)
+
+numbers.remove(0)
+
+numbers.remove(12)
+
+print(numbers, f'length={len(numbers)}')
+
+print(0 in numbers, 1 in numbers, 8 in numbers, 14 in numbers, 19 in numbers)
+
+numbers.insert(0, 0)
+
+numbers.insert(2, 2)
+
+numbers.insert(11, 11)
+
+numbers.insert(12, 12)
+
+print(numbers, f'length={len(numbers)}')
+
+print(0 in numbers, 1 in numbers, 8 in numbers, 14 in numbers, 19 in numbers)
--- concepts/array_list/array_list.py
+++ concepts/array_list/array_list.py
@@ -6,12 +6,57 @@ class ArrayList:
     def __init__(self, *data, length=INITIAL_SIZE):
         if length <= 0:
             length = INITIAL_SIZE
-        self._size = length if length > len(data) else len(data) * 2
-        self._data = [None] * self._size
+        self._capacity = length if length > len(data) else len(data) * 2
+        self._data = [None] * self._capacity
         self._last_element_index = len(data) - 1
         self._iter_index = -1
         for i in range(len(data)):
             self._data[i] = data[i]
+
+    def _is_full(self):
+        return self._capacity == len(self)
+
+    def _grow_size(self):
+        self._data = self._data + ([None] * len(self) * SIZE_FACTOR)
+        self._capacity = len(self._data)
+
+    def append(self, item):
+        if self._is_full():
+            self._grow_size()
+        if self._last_element_index < self._capacity:
+            self._last_element_index += 1
+        self._data[self._last_element_index] = item
+
+    def _is_more_then_half_empty(self):
+        return len(self) * 2 <= self._capacity
+
+    def _reduce_size(self):
+        if len(self) * 2 <= self._capacity:
+            self._data = self._data[0:len(self) * 2]
+        else:
+            self._data = self._data[0:len(self)] + ([None] * len(self) * SIZE_FACTOR)
+        self._capacity = len(self._data)
+
+    def remove(self, item):
+        if self._is_more_then_half_empty():
+            self._reduce_size()
+        remove_index = -1
+        for i in range(0, len(self)):
+            if item == self._data[i]:
+                remove_index = i
+                break
+        if remove_index == -1:
+            raise Exception("No Item found!")
+        self._data = self._data[0:remove_index] + self._data[remove_index+1:]
+        self._last_element_index -= 1
+
+    def insert(self, index, item):
+        if index > len(self) or index < 0:
+            raise IndexError()
+        if self._is_full():
+            self._grow_size()
+        self._data = self._data[0:index] + [item] + self._data[index:]
+        self._last_element_index += 1
 
     def __len__(self):
         return self._last_element_index + 1
@@ -34,16 +79,11 @@ class ArrayList:
             raise IndexError()
         return self._data[i]
 
-    def _is_full(self):
-        return self._size == len(self)
+    def __contains__(self, item):
+        for val in self._data[0:len(self)]:
+            if val == item:
+                return True
+        return False
 
-    def _grow_size(self):
-        self._data = self._data + ([None] * len(self) * SIZE_FACTOR)
-        self._size = len(self._data)
-
-    def append(self, item):
-        if self._is_full():
-            self._grow_size()
-        if self._last_element_index < self._size:
-            self._last_element_index += 1
-        self._data[self._last_element_index] = item
+    def __str__(self):
+        return f'[{", ".join(map(lambda num: str(num), self._data[0:len(self)]))}]'
