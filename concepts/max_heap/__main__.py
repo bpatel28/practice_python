c989f50e68ae42093491f57f8b8f601331ba2965
Brijesh Patel <b.patel9528@gmail.com>
2021-07-24 12:14:34 -0600

    tested MaxHeap

--- concepts/max_heap/__main__.py
+++ concepts/max_heap/__main__.py
@@ -1,10 +1,17 @@
 from concepts.max_heap import MaxHeap
-
+import random
 
 heap = MaxHeap()
+items = []
+for i in range(0, 20, 2):
+    items.append(random.randrange(0, 20, 2))
+    heap.insert(items[-1])
 
-for i in range(20):
-    heap.insert(i)
+for i in range(1, 20, 2):
+    items.append(random.randrange(1, 20, 2))
+    heap.insert(items[-1])
+
+print(items)
 
 for _ in range(20):
     print(heap.remove_max())
